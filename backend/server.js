7f9f3d22cb63996a20540946196fde3402d97cdf
Lone2005Shinobi <khawsemanthan246@gmail.com>
2024-06-29 11:18:07 +0530

    backend cleaned

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,8 @@ import { createServer } from "http";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectdb from "./db/connectDb.js";
-import User from "./db/model.js";
 import cookieParser from "cookie-parser";
-import jwt from "jsonwebtoken";
-
-
+import auth from "./routes/auth.js";
 
 const port = 3000;
 
@@ -23,11 +20,8 @@ const io = new Server(server, {
 });
 
 dotenv.config();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
 connectdb();
 app.use(cookieParser());
 app.use(
@@ -37,120 +31,49 @@ app.use(
     credentials: true,
   })
 );
-
-
-app.post("/api/login", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-
-    const user = await User.findOne({ username: username });
-
-    if (user) {
-      if (user.password === password) {
-        const token = jwt.sign({
-          id: user._id,
-        }, process.env.JWT_SECRET);
-        res.cookie("token", token);
-        return res.status(200).send({message:"login success", data: token});
-      } else {
-        return res.status(401).send("password incorrect");
-      }
-    } else {
-      return res.status(404).send("user not found");
-    }
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send("internal server error");
-  }
-});
-
-app.post("/api/register", async (req, res) => {
-  try {
-    const { name, email, username, password } = req.body;
-
-    const u = await User.findOne({ username: username });
-
-    if (u) {
-      return res.status(409).send("username already exists ");
-    }
-
-    const user = new User({
-      name,
-      email,
-      username,
-      password,
-    });
-
-    await user.save();
-
-    const token = jwt.sign({
-      id: user._id,
-    }, process.env.JWT_SECRET);
-    res.cookie("token", token);
-    return res.status(201).send({message:"user registered successfully", data: token});
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send("internal server error");
-  }
-});
-
-app.post('/api/logout', (req, res) => {
-  res.clearCookie('token');
-  res.json({ message: 'Logout successful' });
-});
-
+app.use("/api", auth);
 
 io.on("connection", (socket) => {
-    // console.log("user connected");
-    // console.log("user id :", socket.id);
+  console.log(`User ${socket.id} connected`);
 
-    socket.on('joinRoom', (room) => {
-      socket.join(room);
-      // console.log(`User ${socket.id} joined room ${room}`);
-    });
+  socket.on("joinRoom", (room) => {
+    socket.join(room);
+    console.log(`User ${socket.id} joined room ${room}`);
+  });
 
-    socket.on('leaveRoom', (room) => {
-      socket.leave(room);
-      // console.log(`User ${socket.id} left room ${room}`);
-    });
+  socket.on("leaveRoom", (room) => {
+    socket.leave(room);
+    console.log(`User ${socket.id} left room ${room}`);
+  });
 
-    socket.on("message", ({room, data}) => {
-        // console.log(data);
-        io.to(room).emit("recieve-message", data);
-    })
+  socket.on("message", ({ room, data }) => {
+    io.to(room).emit("recieve-message", data);
+  });
 
-    socket.on("display-code", ({room,data}) => {
-        console.log(data);
-        io.to(room).emit("recieve-code", data);
-    })
+  socket.on("display-code", ({ room, data }) => {
+    io.to(room).emit("recieve-code", data);
+  });
 
-    socket.on("input-change", ({room,data}) => {
-        console.log(data);
-        io.to(room).emit("recieve-input", data);
-    })
+  socket.on("input-change", ({ room, data }) => {
+    io.to(room).emit("recieve-input", data);
+  });
 
-    socket.on("output-change", ({room, data}) => {
-        console.log(data);
-        io.to(room).emit("recieve-output", data);
-    })
+  socket.on("output-change", ({ room, data }) => {
+    io.to(room).emit("recieve-output", data);
+  });
 
-    socket.on("change-language", ({room, data}) => {
-        console.log(data);
-        io.to(room).emit("recieve-language", data);
-    })
+  socket.on("change-language", ({ room, data }) => {
+    io.to(room).emit("recieve-language", data);
+  });
 
-    socket.on("text-change", ({ room, data }) => {
-      console.log(data);
-      io.to(room).emit("recieve-text", data);
-    });
-
-})
+  socket.on("text-change", ({ room, data }) => {
+    io.to(room).emit("recieve-text", data);
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
-
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
