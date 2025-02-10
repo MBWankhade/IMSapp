943abd9eb8348f28b5089dc4a8c784822081cbbd
MBWankhade <mwankhade718@gmail.com>
2025-02-11 00:20:37 +0530

    Deployment Changes

--- backend/server.js
+++ backend/server.js
@@ -75,6 +75,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`);
 });
